7c390ab746da4f4509868c4739887c5428978ebf
Oliver <oliver.boros.nl@gmail.com>
2022-03-10 10:55:27 +0100

    kk

--- js/quizz.js
+++ js/quizz.js
@@ -1,124 +1,19 @@
 "use strict()";
 
-let vraagNaam = document.getElementById("vraag");
-let quizz = document.getElementById("quizz");
-let submitButton = document.getElementById("submit");
-let stats = document.getElementById("stats");
-let goed = 0;
-let fout = 0;
-let quizVraag = 0;
-let antwrd;
+let quizbox = document.getElementById('quizz');
 
-// let checkvraagCount = 0;
-// begin vragen
-function int(){
-  console.warn("int");
-  document.getElementById("q").addEventListener("click", check);
+let goed;
+let fout;
 
-}
+let vraag01 = ["ant1", "ant2", "ant3", "ant4"];
+let vraag02 = ["ant1", "ant2", "ant3", "ant4"];
+let vraag03 = ["ant1", "ant2", "ant3", "ant4"];
+let vraag04 = ["ant1", "ant2", "ant3", "ant4"];
+let vraag05 = ["ant1", "ant2", "ant3", "ant4"];
+let vraag06 = ["ant1", "ant2", "ant3", "ant4"];
+let vraag07 = ["ant1", "ant2", "ant3", "ant4"];
+let vraag08 = ["ant1", "ant2", "ant3", "ant4"];
+let vraag09 = ["ant1", "ant2", "ant3", "ant4"];
+let vraag10 = ["ant1", "ant2", "ant3", "ant4"];
 
-const q1 =
-  "<input type='radio' id='q' name='q' value='f' /> F <br />" +
-  "<input type='radio' id='q' name='q' value='g' /> G <br />" +
-  "<input type='radio' id='q' name='q' value='f' /> F";
-const q2 =
-  "<input type='radio' id='q' name='q' value='f' /> F <br />" +
-  "<input type='radio' id='q' name='q' value='g' /> G <br />" +
-  "<input type='radio' id='q' name='q' value='f' /> F";
-
-const q3 =
-  "<input type='radio' id='q' name='q' value='f' /> F <br />" +
-  "<input type='radio' id='q' name='q' value='g' /> G <br />" +
-  "<input type='radio' id='q' name='q' value='f' /> F";
-
-const q4 =
-  "<input type='radio' id='q' name='q' value='f' /> F <br />" +
-  "<input type='radio' id='q' name='q' value='g' /> G <br />" +
-  "<input type='radio' id='q' name='q' value='f' /> F";
-
-const q5 =
-  "<input type='radio' id='q' name='q' value='f' /> F <br />" +
-  "<input type='radio' id='q' name='q' value='g' /> G <br />" +
-  "<input type='radio' id='q' name='q' value='f' /> F";
-
-const q6 =
-  "<input type='radio' id='q' name='q' value='f' /> F <br />" +
-  "<input type='radio' id='q' name='q' value='g' /> G <br />" +
-  "<input type='radio' id='q' name='q' value='f' /> F";
-
-const q7 =
-  "<input type='radio' id='q' name='q' value='f' /> F <br />" +
-  "<input type='radio' id='q' name='q' value='g' /> G <br />" +
-  "<input type='radio' id='q' name='q' value='f' /> F";
-
-const q8 =
-  "<input type='radio' id='q' name='q' value='f' /> F <br />" +
-  "<input type='radio' id='q' name='q' value='g' /> G <br />" +
-  "<input type='radio' id='q' name='q' value='f' /> F";
-
-const q9 =
-  "<input type='radio' id='q' name='q' value='f' /> F <br />" +
-  "<input type='radio' id='q' name='q' value='g' /> G <br />" +
-  "<input type='radio' id='q' name='q' value='f' /> F";
-
-const q10 =
-  "<input type='radio' id='q' name='q' value='f' /> F <br />" +
-  "<input type='radio' id='q' name='q' value='g' /> G <br />" +
-  "<input type='radio' id='q' name='q' value='f' /> F";
-
-// eind vragen
-
-// const config = {
-//   type: "doughnut",
-//   data: data,
-// };
-
-function submit() {
-  antwrd = document.getElementById('q').value;
-  if (quizVraag == 0) {
-    submitButton.innerHTML = "Volgende";
-    quizVraag = 1;
-  }
-
-  if (quizVraag == 1) {
-    quizz.innerHTML = q1;
-  } else if (quizVraag == 2) {
-    quizz.innerHTML = q2;
-  } else if (quizVraag == 3) {
-    quizz.innerHTML = q3;
-  } else if (quizVraag == 4) {
-    quizz.innerHTML = q4;
-  } else if (quizVraag == 5) {
-    quizz.innerHTML = q5;
-  } else if (quizVraag == 6) {
-    quizz.innerHTML = q6;
-  } else if (quizVraag == 7) {
-    quizz.innerHTML = q7;
-  } else if (quizVraag == 8) {
-    quizz.innerHTML = q8;
-  } else if (quizVraag == 9) {
-    quizz.innerHTML = q9;
-  } else if (quizVraag == 10) {
-    quizz.innerHTML = q10;
-  } else {
-    console.error("tf u doing");
-  }
-  vraagNaam.innerHTML = "<p>Vraag</p>" + quizVraag;
-  console.warn("vraag", quizVraag);
-  int();
-}
-
-// validatie
-
-function check(antwrd, vrgnum) {
-  console.error("slsll");
-  if (antwrd == "G" && vrgnum == quizVraag) {
-    goed++;
-  } else if (antwrd == "F" && vrgnum == quizVraag) {
-    fout++;
-  } else {
-    console.error("u fucked up lol");
-  }
-  console.log("G:" + goed + " | F:" + fout);
-  int();
-}
+quizbox ;
