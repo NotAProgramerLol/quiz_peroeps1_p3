a5d11e6ac42ab9148573963ea95487e581014740
Oliver <oliver.boros.nl@gmail.com>
2022-03-10 11:43:31 +0100

    Update quizz.js

--- js/quizz.js
+++ js/quizz.js
@@ -1,7 +1,7 @@
 "use strict()";
 
-let goed;
-let fout;
+let goed = 0;
+let fout = 0;
 let huidigeVraag = 1;
 
 let quizbox = document.getElementById("quizz");
@@ -25,29 +25,144 @@ switch (huidigeVraag) {
   case 1:
     q1.innerHTML = vraag01[0];
     q1.value = true;
+    //
     q2.innerHTML = vraag01[1];
+    q2.value = true;
+    //
     q3.innerHTML = vraag01[2];
+    q3.value = true;
+    //
     q4.innerHTML = vraag01[3];
+    q4.value = true;
     break;
   case 2:
+    q1.innerHTML = vraag02[0];
+    q1.value = true;
+    //
+    q2.innerHTML = vraag02[1];
+    q2.value = true;
+    //
+    q3.innerHTML = vraag02[2];
+    q3.value = true;
+    //
+    q4.innerHTML = vraag02[3];
+    q4.value = true;
     break;
   case 3:
+    q1.innerHTML = vraag03[0];
+    q1.value = true;
+    //
+    q2.innerHTML = vraag03[1];
+    q2.value = true;
+    //
+    q3.innerHTML = vraag03[2];
+    q3.value = true;
+    //
+    q4.innerHTML = vraag03[3];
+    q4.value = true;
     break;
   case 4:
+    q1.innerHTML = vraag04[0];
+    q1.value = true;
+    //
+    q2.innerHTML = vraag04[1];
+    q2.value = true;
+    //
+    q3.innerHTML = vraag04[2];
+    q3.value = true;
+    //
+    q4.innerHTML = vraag04[3];
+    q4.value = true;
     break;
   case 5:
+    q1.innerHTML = vraag05[0];
+    q1.value = true;
+    //
+    q2.innerHTML = vraag05[1];
+    q2.value = true;
+    //
+    q3.innerHTML = vraag05[2];
+    q3.value = true;
+    //
+    q4.innerHTML = vraag05[3];
+    q4.value = true;
     break;
   case 6:
+    q1.innerHTML = vraag06[0];
+    q1.value = true;
+    //
+    q2.innerHTML = vraag06[1];
+    q2.value = true;
+    //
+    q3.innerHTML = vraag06[2];
+    q3.value = true;
+    //
+    q4.innerHTML = vraag06[3];
+    q4.value = true;
     break;
   case 7:
+    q1.innerHTML = vraag07[0];
+    q1.value = true;
+    //
+    q2.innerHTML = vraag07[1];
+    q2.value = true;
+    //
+    q3.innerHTML = vraag07[2];
+    q3.value = true;
+    //
+    q4.innerHTML = vraag07[3];
+    q4.value = true;
     break;
   case 8:
+    q1.innerHTML = vraag08[0];
+    q1.value = true;
+    //
+    q2.innerHTML = vraag08[1];
+    q2.value = true;
+    //
+    q3.innerHTML = vraag08[2];
+    q3.value = true;
+    //
+    q4.innerHTML = vraag08[3];
+    q4.value = true;
     break;
   case 9:
+    q1.innerHTML = vraag09[0];
+    q1.value = true;
+    //
+    q2.innerHTML = vraag09[1];
+    q2.value = true;
+    //
+    q3.innerHTML = vraag09[2];
+    q3.value = true;
+    //
+    q4.innerHTML = vraag09[3];
+    q4.value = true;
     break;
   case 10:
+    q1.innerHTML = vraag10[0];
+    q1.value = true;
+    //
+    q2.innerHTML = vraag10[1];
+    q2.value = true;
+    //
+    q3.innerHTML = vraag10[2];
+    q3.value = true;
+    //
+    q4.innerHTML = vraag10[3];
+    q4.value = true;
     break;
 
   default:
+    console.warn("niet iets tussen {0} en {1}", 1, 10);
     break;
+}
+function check(qNummer) {
+  if (qNummer == 1 && q1.value == true) {
+    goed++;
+  } else if (qNummer == 1 && q1.value == false) {
+    fout++;
+  } else {
+    console.error("u fucked up");
+  }
 }
