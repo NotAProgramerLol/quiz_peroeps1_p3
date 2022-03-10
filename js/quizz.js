94d311140797322422628c6705518bf695b8aa09
Oliver <oliver.boros.nl@gmail.com>
2022-03-10 11:21:23 +0100

    Update quizz.js

--- js/quizz.js
+++ js/quizz.js
@@ -1,9 +1,14 @@
 "use strict()";
 
-let quizbox = document.getElementById('quizz');
-
 let goed;
 let fout;
+let huidigeVraag = 1;
+
+let quizbox = document.getElementById("quizz");
+let q1 = document.getElementById("q1");
+let q2 = document.getElementById("q2");
+let q3 = document.getElementById("q3");
+let q4 = document.getElementById("q4");
 
 let vraag01 = ["ant1", "ant2", "ant3", "ant4"];
 let vraag02 = ["ant1", "ant2", "ant3", "ant4"];
@@ -16,4 +21,33 @@ let vraag08 = ["ant1", "ant2", "ant3", "ant4"];
 let vraag09 = ["ant1", "ant2", "ant3", "ant4"];
 let vraag10 = ["ant1", "ant2", "ant3", "ant4"];
 
-quizbox ;
+switch (huidigeVraag) {
+  case 1:
+    q1.innerHTML = vraag01[0];
+    q1.value = true;
+    q2.innerHTML = vraag01[1];
+    q3.innerHTML = vraag01[2];
+    q4.innerHTML = vraag01[3];
+    break;
+  case 2:
+    break;
+  case 3:
+    break;
+  case 4:
+    break;
+  case 5:
+    break;
+  case 6:
+    break;
+  case 7:
+    break;
+  case 8:
+    break;
+  case 9:
+    break;
+  case 10:
+    break;
+
+  default:
+    break;
+}
